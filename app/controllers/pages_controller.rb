6f6785334b2200eb2a348ffc8e39ecfcb06d24e7
Roman Kozvonin <romankozvonin@gmail.com>
2016-12-27 00:07:57 +0400

    26-12-2016 Добавлена глобальная переменная @title для одноименного тэга

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,16 +3,19 @@ class PagesController < ApplicationController
   end
 
   def about
+    @title = t(:about)
   end
   
   def contacts
+    @title = t(:contacts)
   end
 
   def realcraft_190
     @boat = real_craft_190
+    @title = @boat[:name]
   end
 
   def realcraft_200
-    @boat = real_craft_200
+    @boat = real_craft_190#real_craft_200
   end
 end
