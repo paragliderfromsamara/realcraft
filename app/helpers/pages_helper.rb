49caf3c6883047cc59a0b7b68ee932c92eb43388
Roman Kozvonin <romankozvonin@gmail.com>
2017-01-18 21:26:04 +0400

    18-01-2017 добавлены фотографии в раздел О нас

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,3 +1,6 @@
 module PagesHelper
-  
+  def production_photos(ph_name, preview_size = "small")
+    path_name = "/production_photos"
+    return {url: "#{path_name}/#{ph_name}_#{preview_size}.jpg", list: "[#{path_name}/#{ph_name}_small.jpg, small], [#{path_name}/#{ph_name}_medium.jpg, medium], [#{path_name}/#{ph_name}_large.jpg, large], [#{path_name}/#{ph_name}_xlarge.jpg, xlarge]"}
+  end
 end
